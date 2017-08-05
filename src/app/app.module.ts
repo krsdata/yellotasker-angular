a918bb30258e9f6821800810487fa8c0a327256a
kanika <kanikasethi04@gmail.com>
2017-08-05 16:31:51 +0530

    initial yellotasker set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DashBoardComponent } from './dashboard/dashboard.component'
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+   { path: 'dashboard', component: DashBoardComponent },
+   { path: '', component: DashBoardComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component ({
+   selector: 'my-app',
+   templateUrl: './dashboard.component.html' ,
+})
+export class DashBoardComponent  {
+
+}
